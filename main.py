4401248236469bd8433eea3c089089f9307dd342
Kieran Lucas <kieranlucas.work@gmail.com>
2026-02-07 03:53:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+
+
 import sys
 from PySide6.QtWidgets import QApplication, QWidget
 
 app = QApplication(sys.argv)
 
 window = QWidget()
-window.setWindowTitle("PySide6 - Step 1")
-
+window.setWindowTitle("Test")
 window.resize(800, 500)
 window.show()
 
 sys.exit(app.exec())
+
+
+
+
+
+
+
+
